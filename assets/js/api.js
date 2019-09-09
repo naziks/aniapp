41cb6e21bd5987757696932fdb553f59c6db87aa
naziks <47501073+naziks@users.noreply.github.com>
2019-09-09 23:25:40 +0300

    Fix

--- assets/js/api.js
+++ assets/js/api.js
@@ -48,8 +48,8 @@ const api = (method = false, params = {}, callback = (r)=>{}, quiet=true) => {
 	}
 	if(!quiet)
 		loading(true);
-	let url = 'http://3.3.3.3/animevost.app/api/'
-	// let url = 'https://naziksbots.000webhostapp.com/animevost/api/'
+	// let url = 'http://3.3.3.3/animevost.app/api/'
+	let url = 'https://naziksbots.000webhostapp.com/animevost/api/'
 	url = url + method+'?'+build_params(params)
 
 	$.ajax(url)
